922eddfa381d4c75ad4c73295f07d58555920489
wujiabao <wujiabao@leateckgroup.com>
2023-10-17 18:12:42 +0800

    -fix:初始化家在

---
+++ my-react-core/src/App.js
@@ -0,0 +1,16 @@
+
+import { useState } from "react";
+import './App.css';
+
+function App() {
+
+  const [a, setA] = useState('b');
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
